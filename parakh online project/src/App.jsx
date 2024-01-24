849c66a7f48a2ca39e0fbd70fe82a7c5fe1ab381
mohak64 <mohak.bansal@createbytes.com>
2024-01-24 16:53:07 +0530

    adding functionality

--- parakh online project/src/App.jsx
+++ parakh online project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,23 +7,31 @@ import Product from "./Product";
 
 function App() {
   const ProductData = [...data.data];
-  console.log(ProductData);
+  const [cart, setCart] = useState([]);
+
   return (
     <>
       <div className='fs-1 h2'>Parakh Online Assignment</div>
-      {ProductData.map((item) => (
-        <div className='container mt-5 h3 text-start'>{item.name}
-          <div className='border border-dark border-2'>
-          </div>
-          <div className='d-flex gap-4 my-4'>
-            {item.productList.map((product) => (
-              <Product
-              />
-            ))}
+      {ProductData.map((item, idx) => (
+        <div key={item.name}>
+          <div className='container mt-5 h3 text-start'>{item.name}
+            <div className='border border-dark border-2'>
+            </div>
+            <div className='d-flex gap-4 my-4'>
+              {item.productList.map((product) => (
+
+                < Product
+                  key={product.id}
+                  product={product}
+                  cart={cart}
+                  setCart={setCart}
+                />
+              ))}
+            </div>
           </div>
         </div>
-
       ))}
+
     </>
   )
 }
